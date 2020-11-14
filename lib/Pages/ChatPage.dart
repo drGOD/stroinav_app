c9531630eaec63bb8eb779ce24d0eb59b081eaca
Акулов Никита Андройдович <UnbrokenKnight@yandex.ru>
2020-11-14 20:31:58 +0300

    Освободил ChatPage, перенёс карту в MapPage

--- lib/Pages/ChatPage.dart
+++ lib/Pages/ChatPage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
 
 class PageOne extends StatefulWidget {
   PageOne({Key key, this.userId, this.onSignedOut}) : super(key: key);
@@ -29,27 +27,7 @@ class _PageOneState extends State<PageOne> {
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
-          body:  new FlutterMap(
-        options: new MapOptions(
-        center: new LatLng(55.785811, 37.4491582),
-        zoom: 13.0,
-      ),
-        layers: [
-          new TileLayerOptions(
-            urlTemplate: "http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png"//"https://api.mapbox.com/styles/v1/unbrokenknight/ckhgk4unr0z5g19mw3yy7ctdj/wmts?access_token=pk.eyJ1IjoidW5icm9rZW5rbmlnaHQiLCJhIjoiY2toZ2gxZXBjMHpiMjM1bDZ2aGxxMjFpeCJ9.l_g8F2SX1h-xdlCEATV-ew",
-            /*additionalOptions: {
-              'accessToken': 'pk.eyJ1IjoidW5icm9rZW5rbmlnaHQiLCJhIjoiY2toZ2gxZXBjMHpiMjM1bDZ2aGxxMjFpeCJ9.l_g8F2SX1h-xdlCEATV-ew',
-              'id': 'mapbox.mapbox-streets-v8',
-            },*/
-          ),
-          new MarkerLayerOptions(
-              markers: [
-                new Marker(
-                    width: 40.0,
-                    height: 40.0,
-                    point: new LatLng(55.785811, 37.4491582),
-                    builder: (ctx) => new Container(child: new FlutterLogo()))
-              ])]),
+
         ),
       ),
     );
--- lib/Pages/MapPage.dart
+++ lib/Pages/MapPage.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class PageTwo extends StatefulWidget {
   PageTwo({Key key, this.userId, this.onSignedOut}) : super(key: key);
@@ -10,9 +12,9 @@ class PageTwo extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => new _PageTwoState(
-        userId: userId,
-        onSignedOut: onSignedOut,
-      );
+    userId: userId,
+    onSignedOut: onSignedOut,
+  );
 }
 
 class _PageTwoState extends State<PageTwo> {
@@ -29,7 +31,7 @@ class _PageTwoState extends State<PageTwo> {
 
     _geolocator = Geolocator();
     LocationOptions locationOptions =
-        LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
+    LocationOptions(accuracy: LocationAccuracy.high, distanceFilter: 1);
 
     updateLocation();
 
@@ -59,13 +61,38 @@ class _PageTwoState extends State<PageTwo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: Center(
+      body:
+      _map(),
+      /*Center(
+          _qrBlock(),
           child: Text(
               'Latitude: ${_position != null ? _position.latitude.toString() : '0'},'
-              ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}')),
+              ' Longitude: ${_position != null ? _position.longitude.toString() : '0'}')),*/
     );
   }
+
+  Widget _map() {
+    return FlutterMap(
+        options: new MapOptions(
+          center: new LatLng(55.785811, 37.4491582),
+          zoom: 13.0,
+        ),
+        layers: [
+          new TileLayerOptions(
+              urlTemplate: "http://tiles.maps.sputnik.ru/{z}/{x}/{y}.png"//"https://api.mapbox.com/styles/v1/unbrokenknight/ckhgk4unr0z5g19mw3yy7ctdj/wmts?access_token=pk.eyJ1IjoidW5icm9rZW5rbmlnaHQiLCJhIjoiY2toZ2gxZXBjMHpiMjM1bDZ2aGxxMjFpeCJ9.l_g8F2SX1h-xdlCEATV-ew",
+            /*additionalOptions: {
+              'accessToken': 'pk.eyJ1IjoidW5icm9rZW5rbmlnaHQiLCJhIjoiY2toZ2gxZXBjMHpiMjM1bDZ2aGxxMjFpeCJ9.l_g8F2SX1h-xdlCEATV-ew',
+              'id': 'mapbox.mapbox-streets-v8',
+            },*/
+          ),
+          new MarkerLayerOptions(
+              markers: [
+                new Marker(
+                    width: 40.0,
+                    height: 40.0,
+                    point: new LatLng(55.785811, 37.4491582),
+                    builder: (ctx) => new Container(child: new FlutterLogo()))
+              ])]);
+  }
+
 }
